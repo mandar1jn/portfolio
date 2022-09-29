1ebebeb510c5ff37caae3fd500e2f67043853dec
Mandar1jn <Marijnkneppers@gmail.com>
2022-09-29 21:29:42 +0200

    Added a recent posts part to my homepage

--- src/utils/posts.ts
+++ src/utils/posts.ts
@@ -1,3 +1,5 @@
+import type { MarkdownInstance } from "astro";
+
 export type FrontMatter = {
 	title: string;
 	description: string;
@@ -5,3 +7,11 @@ export type FrontMatter = {
 	imgSrc: string;
 	imgAlt: string;
 }
+
+export const sortByDate = (posts: MarkdownInstance<FrontMatter>[]) => {
+	return posts.sort(
+	  (a, b) =>
+		new Date(b.frontmatter.pubDate).valueOf() -
+		new Date(a.frontmatter.pubDate).valueOf()
+	);
+  };
