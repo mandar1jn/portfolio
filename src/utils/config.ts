48d781bfcb71ce37fe5dc8d0f4505e6354da91da
Mandar1jn <Marijnkneppers@gmail.com>
2022-10-14 20:27:48 +0200

    Working on a real config

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,1 +1,12 @@
-export const siteName = "Marijn Kneppers";
+let config =
+{
+	siteName: "Marijn Kneppers",
+	author: {
+		firstName: "Marijn",
+		lastName: "Kneppers",
+		fullName: "Marijn Kneppers",
+		birthData: new Date(Date.parse("2006-08-27T06:30:00+02:00"))
+	}
+}
+
+export default config;
