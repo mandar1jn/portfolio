53200df0b886dcf47d2e67d2c6a767166a0a4f2c
Mandar1jn <Marijnkneppers@gmail.com>
2023-01-01 18:43:46 +0100

    Correctly parse date in content schema

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,11 +1,14 @@
 import { z, defineCollection } from "astro:content"
 
+
+
 const blog = defineCollection({
 	schema: {
 		title: z.string(),
 		description: z.string(),
-		// add .datetime() and fix later
-		pubDate: z.string(),
+		pubDate: z.preprocess((arg) => {
+			if (typeof arg == "string" || arg instanceof Date) return new Date(arg);
+		  }, z.date()),
 		imgSrc: z.string(),
 		imgAlt: z.string(),
 		draft: z.boolean().optional()
