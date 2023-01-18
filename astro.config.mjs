4949dc00269212b4620107c69f1330d9a9f9920a
Mandar1jn <Marijnkneppers@gmail.com>
2023-01-18 21:11:16 +0100

    Switched to mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,20 +1,20 @@
 import { defineConfig } from 'astro/config';
 import sitemap from "@astrojs/sitemap";
 import image from "@astrojs/image";
-
 import tailwind from "@astrojs/tailwind";
 import remarkGfm from 'remark-gfm';
 
 // https://astro.build/config
+import mdx from "@astrojs/mdx";
+
+// https://astro.build/config
 export default defineConfig({
-  site: "https://marijnkneppers.dev",
-  integrations: [sitemap(), image({
-    serviceEntryPoint: '@astrojs/image/sharp'
-  }), tailwind()],
-  output: "static",
-  markdown: {
-	syntaxHighlight: false,
-	extendDefaultPlugins: true,
-	remarkPlugins: [remarkGfm]
-  },
+	site: "https://marijnkneppers.dev",
+	integrations: [sitemap(), image({
+		serviceEntryPoint: '@astrojs/image/sharp'
+	}), tailwind(), mdx()],
+	output: "static",
+	markdown: {
+		syntaxHighlight: false,
+	}
 });
