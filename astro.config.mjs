c23c4b1099602d4fd17e538b0df639f0a320d596
Mandar1jn <Marijnkneppers@gmail.com>
2023-01-18 20:19:33 +0100

    Upgraded to astro 2.0

--- astro.config.mjs
+++ astro.config.mjs
@@ -17,7 +17,4 @@ export default defineConfig({
 	extendDefaultPlugins: true,
 	remarkPlugins: [remarkGfm]
   },
-  experimental: {
-	contentCollections: true
-  }
 });
--- src/content/config.ts
+++ src/content/config.ts
@@ -3,25 +3,25 @@ import { z, defineCollection } from "astro:content"
 
 
 const blog = defineCollection({
-	schema: {
+	schema: z.object({
 		title: z.string(),
 		description: z.string(),
 		pubDate: z.coerce.date(),
 		imgSrc: z.string(),
 		imgAlt: z.string(),
 		draft: z.boolean().optional()
-	}
+	})
 })
 
 const projects = defineCollection({
-	schema: {
+	schema: z.object({
 		title: z.string(),
 		description: z.string(),
 		imgSrc: z.string(),
 		imgAlt: z.string(),
 		pubDate: z.coerce.date(),
 		tags: z.string().array()
-	}
+	})
 })
 
 export const collections = {
